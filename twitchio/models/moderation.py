68db1ec95bb24ab79f581c9b1a4b21bc10c07272
chillymosh <86857777+chillymosh@users.noreply.github.com>
2026-07-05 14:53:06 +0100

    Make ShieldModeStatus attrs optional

--- twitchio/models/moderation.py
+++ twitchio/models/moderation.py
@@ -392,20 +392,24 @@ class ShieldModeStatus:
     ----------
     active: bool
         Whether Shield Mode is active. Is true if Shield Mode is active;
-    moderator: PartialUser
-        The moderator that last activated Shield Mode.
-    last_activated_at: datetime.datetime
-        When Shield Mode was last activated.
+    moderator: PartialUser | None
+        The moderator that last activated Shield Mode. Is ``None`` if existing Shield Mode setting is the same as updated setting.
+    last_activated_at: datetime.datetime | None
+        When Shield Mode was last activated. Is ``None`` if existing Shield Mode setting is the same as updated setting.
     """
 
     __slots__ = ("active", "last_activated_at", "moderator")
 
     def __init__(self, data: ShieldModeStatusResponseData, *, http: HTTPClient) -> None:
         self.active: bool = bool(data["is_active"])
-        self.moderator: PartialUser = PartialUser(
-            data["moderator_id"], data["moderator_login"], data["moderator_name"], http=http
+        self.moderator: PartialUser | None = (
+            PartialUser(data["moderator_id"], data["moderator_login"], data["moderator_name"], http=http)
+            if data["moderator_id"]
+            else None
         )
-        self.last_activated_at: datetime.datetime = parse_timestamp(data["last_activated_at"])
+        self.last_activated_at: datetime.datetime | None = (
+            parse_timestamp(data["last_activated_at"]) if data["last_activated_at"] else None
+        )
 
     def __repr__(self) -> str:
         return (
