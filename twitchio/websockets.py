8d674201725c0b6fc8a9e6aaa70de2aff05dde77
EvieePy <29671945+EvieePy@users.noreply.github.com>
2026-08-09 15:02:13 +1000

    Continued websockets redesign

--- twitchio/websockets.py
+++ twitchio/websockets.py
@@ -28,7 +28,7 @@ import dataclasses
 import logging
 import time
 from types import MappingProxyType
-from typing import TYPE_CHECKING, Any, Self, cast
+from typing import TYPE_CHECKING, Any, cast
 
 from picows import (
     WSCloseCode,
@@ -64,6 +64,7 @@ LOGGER: logging.Logger = logging.getLogger(__name__)
 WSS_URL: str = "wss://eventsub.wss.twitch.tv/ws"
 MIN_KEEP_ALIVE: int = 10
 MAX_KEEP_ALIVE: int = 600
+CLEANUP_TIMEOUT: int = 5
 
 FAIL_CODES: tuple[int | WSCloseCode, ...] = (
     WSCloseCode.PROTOCOL_ERROR,
@@ -93,7 +94,7 @@ class WebsocketManager:
         self._client = client
         self._sockets: dict[str, Websocket] = {}
         self._tasks: set[asyncio.Task[None]] = set()
-        self._keepalive = min(max(keepalive_timeout, MIN_KEEP_ALIVE), MAX_KEEP_ALIVE)
+        self._keepalive = int(min(max(keepalive_timeout, MIN_KEEP_ALIVE), MAX_KEEP_ALIVE))
 
     @property
     def sockets(self) -> MappingProxyType[str, Websocket]:
@@ -102,29 +103,31 @@ class WebsocketManager:
     def get_socket(self, session_id: str, /) -> Websocket | None:
         return self._sockets.get(session_id)
 
-    async def _handle_reconnect(self, socket: Websocket, *, error: Exception | None) -> None:
-        assert socket.transport and socket.listener
-        listener = socket.listener
+    async def _handle_reconnect(self, socket: Websocket, listener: WebsocketFrame, *, error: Exception | None) -> None:
+        if socket._closing or listener._close_expected or listener is not socket.listener:
+            return
 
         code = listener._close_code
         reason = listener._close_reason
 
         if code is None:
-            LOGGER.warning("%r received an unknown close code (%s: %d). Attempting to reconnect...", socket, error, code)
+            LOGGER.warning("%r received an unknown close code (%s: %s). Attempting to reconnect...", socket, error, code)
             await self.reconnect_socket(socket)
             return
 
         if code in FAIL_CODES:
-            LOGGER.error("%r was disconnected forcefully by Twitch. Cannot reconnect: %s (%d).", socket, reason, code)
+            LOGGER.error("%r was disconnected forcefully by Twitch. Cannot reconnect: %s (%s).", socket, reason, code)
             await self.close_socket(socket)
             return
 
-        LOGGER.info("%r was disconnected (%d). Attempting to reconnect...", socket, code)
+        LOGGER.info("%r was disconnected (%s). Attempting to reconnect...", socket, code)
         await self.reconnect_socket(socket=socket, soft=code in SOFT_FAIL_CODES)
 
     async def reconnect_socket(self, socket: Websocket, *, soft: bool = False) -> ...: ...
 
-    async def open_socket(self) -> ...: ...
+    async def open_socket(self) -> ...:
+        ws = Websocket(self)
+        await ws.open(keepalive=self._keepalive)
 
     async def batch_open(self) -> ...: ...
 
@@ -139,6 +142,8 @@ class WebsocketManager:
     async def _dispatch_session_reconnect(self, socket: Websocket, *, data: ReconnectMessage, received_at: float) -> ...: ...
 
     async def _dispatch_session_welcome(self, socket: Websocket, *, data: WelcomeMessage, received_at: float) -> ...:
+        LOGGER.debug("Received 'session_welcome' on %r: %s", socket, data)
+
         socket._session_id = data["payload"]["session"]["id"]
         socket.set_ready()
 
@@ -149,40 +154,50 @@ class WebsocketManager:
         self._tasks.add(task)
         task.add_done_callback(self._tasks.discard)
 
-    async def _socket_channel(self, socket: Websocket) -> None:
-        messages = socket._messages
+    async def _process_message(self, socket: Websocket, *, data: tuple[float, Any]) -> None:
+        try:
+            received, message = data
+            metadata: MetaData = message["metadata"]
+            message_type: MessageTypes = metadata["message_type"]
+        except Exception as e:
+            LOGGER.error("Unknown exception processing message in %r: %s. Data: %s", socket, e, data, exc_info=e)
+            return
 
-        while True:
-            data: tuple[float, Any] = await messages.get()
+        if message_type == "session_keepalive":
+            LOGGER.debug("Received 'session_keepalive' on %r: %s", socket, data)
+            return
+
+        elif message_type == "notification":
+            self._wrap_notification(socket, message=message)
+            return
+
+        coro = getattr(self, f"_dispatch_{message_type}", None)
+        if not coro:
+            LOGGER.warning("Unknown message type received for %r: %s", socket, data)
+            return
+
+        await coro(socket, data=message, received_at=received)
+
+    async def _socket_channel(self, socket: Websocket, listener: WebsocketFrame) -> None:
+        messages = listener._messages
+
+        while not listener._stop_reading:
+            try:
+                data: tuple[float, Any] = await messages.get()
+            except asyncio.QueueShutDown:
+                break
 
             try:
-                received, message = data
-                metadata: MetaData = message["metadata"]
-                message_type: MessageTypes = metadata["message_type"]
-            except Exception as e:
-                LOGGER.error("Unknown exception processing message in %r: %s. Data: %s", socket, e, data, exc_info=e)
-                continue
-
-            if message_type == "session_keepalive":
-                LOGGER.debug("Received 'session_keepalive' on %r: %s", socket, data)
-                continue
-
-            elif message_type == "notification":
-                self._wrap_notification(socket, message=message)
-                continue
-
-            coro = getattr(self, f"_dispatch_{message_type}", None)
-            if not coro:
-                LOGGER.warning("Unknown message type received for %r: %s", socket, data)
-                continue
-
-            try:
-                await coro(socket, data=message, received_at=received)
+                await self._process_message(socket, data=data)
             except asyncio.CancelledError:
                 raise
             except Exception:
-                ...
                 # TODO: Handling/Logging...
+                ...
+            finally:
+                messages.task_done()
+
+        LOGGER.debug("%r message consumer has gracefully terminated.", socket)
 
 
 class WebsocketFrame(WSListener):
@@ -193,8 +208,13 @@ class WebsocketFrame(WSListener):
         self._close_code: WSCloseCode | None = None
         self._close_reason: str | None = None
         self._close_expected: bool = False
+        self._stop_reading: bool = False
+        self._messages: asyncio.Queue[tuple[float, Any]] = asyncio.Queue()
 
     def on_ws_frame(self, transport: WSTransport, frame: WSFrame) -> None:
+        if self._stop_reading:
+            return
+
         if frame.msg_type is WSMsgType.TEXT:
             try:
                 data = JSON_LOADS(frame.get_payload_as_bytes())
@@ -202,8 +222,12 @@ class WebsocketFrame(WSListener):
                 LOGGER.error("Unable to process message for %r: %r", self._socket, frame, exc_info=e)
                 return
 
+            try:
+                self._messages.put_nowait((time.monotonic(), data))
+            except asyncio.QueueShutDown:
+                return
+
             self._socket.ack()
-            self._socket._messages.put_nowait((time.monotonic(), data))
             return
 
         if frame.msg_type is not WSMsgType.CLOSE:
@@ -213,12 +237,22 @@ class WebsocketFrame(WSListener):
         self._close_reason = frame.get_close_reason()
 
         LOGGER.debug("%r received CLOSE %s: %s", self._socket, self._close_code, self._close_reason)
+        self._stop_reading = True
         transport.send_close(WSCloseCode.OK)
         transport.disconnect()
 
 
 class Websocket:
-    __slots__ = ("_channel_task", "_last_ack", "_messages", "_ready_event", "_session_id", "_watcher_task", "container")
+    __slots__ = (
+        "_channel_task",
+        "_closing",
+        "_last_ack",
+        "_opening",
+        "_ready_event",
+        "_session_id",
+        "_watcher_task",
+        "container",
+    )
 
     def __init__(self, manager: WebsocketManager) -> None:
         self.container: WSContainer = WSContainer(manager=manager, listener=None, transport=None)
@@ -226,10 +260,10 @@ class Websocket:
         self._session_id: str | None = None
         self._ready_event = asyncio.Event()
         self._last_ack: float = time.monotonic()
-
-        self._messages: asyncio.Queue[tuple[float, Any]] = asyncio.Queue()
         self._channel_task: asyncio.Task[None] | None = None
         self._watcher_task: asyncio.Task[None] | None = None
+        self._opening: bool = False
+        self._closing: bool = False
 
     @property
     def manager(self) -> WebsocketManager:
@@ -262,21 +296,76 @@ class Websocket:
     def listener_factory(self) -> WebsocketFrame:
         return WebsocketFrame(self)
 
-    async def open(self, *, uri: str = MISSING) -> Self:
+    async def open(self, *, uri: str = MISSING, keepalive: int = MIN_KEEP_ALIVE) -> None:
+        if self._closing or self._opening:
+            # TODO: Error?
+            return
+
+        self._opening = True
         self._ready_event.clear()
 
-        uri = uri if uri is not MISSING else WSS_URL
-        transport, listener = await ws_connect(url=uri, ws_listener_factory=self.listener_factory)
+        if uri is MISSING:
+            uri = f"{WSS_URL}?keepalive_timeout_seconds={keepalive}"
 
+        transport, listener = await ws_connect(url=uri, ws_listener_factory=self.listener_factory)
         self.container.transport = transport
         self.container.listener = cast("WebsocketFrame", listener)
         self._start_background_tasks()
 
-        return self
+        self._opening = False
 
-    async def close(self) -> None: ...
+    async def close(self, *, code: WSCloseCode = WSCloseCode.OK, force: bool = False) -> None:
+        if self._closing or self._opening:
+            return
 
-    def cleanup(self) -> None: ...
+        self._closing = True
+        if self.transport and not self.transport.is_disconnected:
+            await self._cleanup_transport(code)
+
+        if self.listener:
+            self.listener._close_expected = True
+
+            try:
+                await self.drain(self.listener, immediate=force)
+            except TimeoutError:
+                LOGGER.debug("%r message consumer could not drain gracefully. Disregarding any remaining messages.", self)
+            finally:
+                self.listener._stop_reading = True
+
+        self.cleanup()
+
+    async def _cleanup_transport(self, code: WSCloseCode) -> None:
+        assert self.transport
+        self.transport.send_close(code)
+        self.transport.disconnect()
+
+        try:
+            async with asyncio.timeout(CLEANUP_TIMEOUT):
+                await self.transport.wait_disconnected()
+        except TimeoutError:
+            pass
+
+    def cleanup(self) -> None:
+        current = asyncio.current_task()
+
+        for task in (self._channel_task, self._watcher_task):
+            if task is not None and task is not current:
+                task.cancel()
+
+        self._ready_event.clear()
+        self._session_id = None
+        self.container.listener = None
+        self.container.transport = None
+        self._channel_task = None
+        self._watcher_task = None
+
+        self._closing = False
+
+    async def drain(self, listener: WebsocketFrame, *, immediate: bool = False) -> None:
+        listener._messages.shutdown(immediate=immediate)
+
+        async with asyncio.timeout(CLEANUP_TIMEOUT):
+            await listener._messages.join()
 
     async def resume(self) -> None: ...
 
@@ -287,7 +376,7 @@ class Websocket:
         LOGGER.debug("Starting background tasks on websocket %r", self)
 
         assert self.listener
-        self._channel_task = asyncio.create_task(self.manager._socket_channel(self))
+        self._channel_task = asyncio.create_task(self.manager._socket_channel(self, self.listener))
         self._watcher_task = asyncio.create_task(self._watcher(self.listener))
 
     async def _watcher(self, listener: WebsocketFrame) -> None:
@@ -300,14 +389,16 @@ class Websocket:
             error = e
 
         if listener._close_expected:
-            LOGGER.debug("Ignoring excpected closure in %r.", self)
+            LOGGER.debug("Ignoring expected closure in %r.", self)
             return
 
         if listener is not self.listener:
             LOGGER.debug("Ignoring closure from a superseded listener in %r. Twitch has likely resumed this socket.", self)
             return
 
-        await self.manager._handle_reconnect(self, error=error)
+        task = asyncio.create_task(self.manager._handle_reconnect(self, listener=listener, error=error))
+        self.manager._tasks.add(task)
+        task.add_done_callback(self.manager._tasks.discard)
 
     def ack(self) -> None:
         self._last_ack = time.monotonic()
