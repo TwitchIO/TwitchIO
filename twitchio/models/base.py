5ae5405072b29d08c25f00381991569fa9d0a815
EvieePy <29671945+EvieePy@users.noreply.github.com>
2026-08-12 13:55:17 +1000

    Add base model class and new logic

---
+++ twitchio/models/base.py
@@ -0,0 +1,40 @@
+"""MIT License
+
+Copyright (c) 2025 - Present Evie. P., Chillymosh and TwitchIO
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from typing import Any, Self
+
+from ..http import HTTPClient
+
+
+class BaseModel:
+    _http: HTTPClient
+
+    def __new__(cls, *args: Any, **kwargs: Any) -> Self:
+        http = kwargs.pop("http_", None)
+        if http is None:
+            raise TypeError(f"{cls.__qualname__} expected a 'http_' keyword-argument which is missing.")
+
+        inst = super().__new__(cls)
+        inst._http = http
+
+        return inst
